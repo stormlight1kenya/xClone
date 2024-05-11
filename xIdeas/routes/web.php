53b7a98bfd9bc020401f7cbfced69bdeec1aead9
stormlight1kenya <112975021+stormlight1kenya@users.noreply.github.com>
2024-05-11 20:19:30 +0300

    connect to database
    connecting to mysql database

---
+++ xIdeas/routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TermsController;
+use Illuminate\Support\Facades\Route;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [DashboardController::class, 'index']);
+
+Route::get('/terms', [TermsController::class, 'index']);
+
+//Route::get('/', [ProfileController::class, 'index']);
